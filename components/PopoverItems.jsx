8063374061c65fb8aefd313e2375ec635dd38ef9
Issa Gruber <issa.gruber88@gmail.com>
2023-11-03 18:23:35 +1100

    fixed menu

--- components/PopoverItems.jsx
+++ components/PopoverItems.jsx
@@ -63,7 +63,7 @@ export default function PopoverItems() {
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <Popover.Panel className="absolute inset-x-0 top-full text-gray-500 sm:text-sm">
+                  <Popover.Panel className="absolute inset-x-0 top-full text-gray-500 sm:text-sm z-[1]">
                     {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
                     <div
                       className="absolute inset-0 top-1/2 bg-white shadow"
