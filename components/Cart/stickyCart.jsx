b11a5f8ed52ed53953eec54375e0c1f7167dbff2
Issa Gruber <issa.gruber88@gmail.com>
2023-11-20 13:34:07 +1100

    mobile sizing of cart icon

--- components/Cart/stickyCart.jsx
+++ components/Cart/stickyCart.jsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default function StickyCart() {
   return (
-    <div className="fixed z-50 lg:top-4 lg:right-4 md:right-4 top-4 right-16">
+    <div className="fixed z-50 lg:top-4 lg:right-4 md:right-4 top-4 right-10">
       <span className="">
         <Suspense fallback={<OpenCart />}>
           <Cart />
