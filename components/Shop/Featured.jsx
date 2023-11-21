0be6737e73f429b15c870087596ddb988a5d3219
Issa Gruber <issa.gruber88@gmail.com>
2023-11-21 13:24:05 +1100

    new photo for featured artist

--- components/Shop/Featured.jsx
+++ components/Shop/Featured.jsx
@@ -7,10 +7,10 @@ export default function Featured() {
         <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
             <div>
-              <div className="aspect-h-1 aspect-w-1 overflow-hidden bg-black-100">
+              <div className="aspect-square overflow-hidden bg-black-100">
                 <img
-                  src="https://ipfs.filebase.io/ipfs/QmYYVnYkPqtePzSc5jKxRK3AX3q4x1c8cFU4ZzCLzgEiWV"
-                  alt="Black kettle with long pour spot and angled body on marble counter next to coffee mug and pour-over system."
+                  src="https://ipfs.filebase.io/ipfs/QmbVuKk4UdELdq7qnBhUHk4xEkBxAkUDH1Pqh9htv2MFEQ"
+                  alt="Kyle Steed painting in The Graham Residence living room during the day."
                   className="h-full w-full object-cover object-center"
                 />
               </div>
@@ -33,11 +33,7 @@ export default function Featured() {
                   sense of home gave way to his singular visual language that
                   draws on a range of influences including meditations, loss,
                   sought utopias, memory, identity as well as longing and
-                  understanding of self. Kyle Andrew Steed (b. 1982; Savannah,
-                  GA) is a self-taught painter, photographer, muralist and
-                  typographer. For the first fourteen years of his life he moved
-                  across the southern US before his family settled in Nashville,
-                  Tennessee.
+                  understanding of self.
                 </p>
               </div>
               <div>
