c77b88ca7a7f494b6b233410f92300ee188785b6
Issa Gruber <issa.gruber88@gmail.com>
2023-11-27 15:18:56 +1100

    favicon

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="shortcut icon" href="../public/images/favicon_io/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="../public/images/favicon_io/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="../public/images/favicon_io/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="../public/images/favicon_io/favicon-16x16.png"/>
+      <link rel="shortcut icon" href="./images/favicon_io/icon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon_io/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon_io/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon_io/favicon-16x16.png"/>
       </head>
       <body className={`${inter.children}`}>
         <div className="relative fixed">
