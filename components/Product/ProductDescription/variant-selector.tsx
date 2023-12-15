26a45784c806fc5e8229226e0f5909694a1a62e5
Issa Gruber <issa.gruber88@gmail.com>
2023-12-15 16:56:35 +1100

    variant spacing

--- components/Product/ProductDescription/variant-selector.tsx
+++ components/Product/ProductDescription/variant-selector.tsx
@@ -45,7 +45,7 @@ export function VariantSelector({
   return options.map((option) => (
     <dl className="mb-10" key={option.id}>
       <dt className="mb-4 text-sm uppercase tracking-wide">{option.name}</dt>
-      <dd className="flex flex-wrap gap-3 justify-around sm:justify-start">
+      <dd className="flex flex-wrap gap-4 sm:gap-3">
         {option.values.map((value) => {
           const optionNameLowerCase = option.name.toLowerCase();
 
