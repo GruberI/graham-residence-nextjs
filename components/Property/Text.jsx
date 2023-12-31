9cc48671479ed0871083605592ec97aa4250a9e2
Issa Gruber <issa.gruber88@gmail.com>
2023-12-31 13:12:38 +1030

    spacing

--- components/Property/Text.jsx
+++ components/Property/Text.jsx
@@ -4,7 +4,7 @@ export default function Text() {
         <div className="mx-auto text-center">
           <div className="inset-x-0 bottom-0 sm:static sm:self-stretch border-t border-b border-black py-16 px-10">
           
-              <h1 className={`text-md tracking-widest`}>
+              <h1 className={`text-lg tracking-widest`}>
               The Graham Residence is a mid-century modern retreat situated 4,000 feet above sea level on 20 private acres surrounded by massive weathered boulders, ancient junipers, piñon pines and desert oaks in the heart of Yucca Valley. Serenity and solitude are pervasive and the only neighbors are blue jays, hummingbirds, and the occasional coyote.  This is Mojave Desert at its finest.
               </h1>
 
