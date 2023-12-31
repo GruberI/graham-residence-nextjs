61efbbc97aa34d240d4281ecab1f6ffb0bc20d09
Issa Gruber <issa.gruber88@gmail.com>
2023-12-31 11:08:30 +1030

    missed exhibition titles, update font

--- components/Exhibition/ThaiMainhard/Header.jsx
+++ components/Exhibition/ThaiMainhard/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
       <div className="bg-white py-26 lg:px-6 sm:px-2 sm:pt-20 pt-48">
       {/* <div className="bg-black h-20 w-screen inset-x-0 top-0 absolute z-10"></div> */}
         <div className="mx-auto max-w-4xl text-center">
-          <h1 className="font-bold sm:text-5xl text-3xl sm:pb-6 pb-1 tracking-widest sm:mt-6 ">DESERT SPRING</h1>
+          <h1 className="font-bold sm:text-5xl text-3xl sm:pb-6 pb-1 tracking-widest sm:mt-6 font-[cormorant]">DESERT SPRING</h1>
           <p className="sm:pb-8 pb-3 pt-2 sm:pt-0 sm:text-6xl text-2xl">A Thai Mainhard Collection</p>
           <p className="pb-8 sm:pb-0 sm:text-base text-xs">EXHIBITION BY THE GRAHAM RESIDENCE</p>
         </div>
