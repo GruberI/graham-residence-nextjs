6359b98f8b8008ac14431148c5182a07a2596568
Issa Gruber <issa.gruber88@gmail.com>
2023-12-31 12:23:27 +1030

    fix dynamic header for shop pages

--- components/ShopAll/Home/Header.jsx
+++ components/ShopAll/Home/Header.jsx
@@ -2,16 +2,34 @@ const imageByHandle = [
   {
     imageHandle: "atelier-saucier",
     src: "https://ipfs.filebase.io/ipfs/QmZ3xMFpfF5q6bT1WDv9eGCCswDKzf69ZwYrEHUMJcmV2c/QmbPMLL9ccr3Bf769zGT2EaDpHmy3VHAMQpWMc8x9zVdTx",
+    alt: "Atalier Saucier set table",
+  },
+  {
+    imageHandle: "sophie-lou-jacobsen",
+    src: "https://ipfs.filebase.io/ipfs/QmbvZNPAsQda7iz8DV5jbp8x4iMeBrUUDvkYoZJA3CUbKy/QmYEiT9GLBmVUG94r4rFSNUQXog4BP7xZzT4TSofPEnFMA",
+    alt: "Sophie Lou Jacobsen set table with glass vases",
   },
 ];
 export default function ShopAllHomeHeader({ productHandle }) {
   return (
     <div className="relative isolate overflow-hidden sm:pt-24 pt-40">
-      <img
-        src={productHandle === 'atelier-saucier' ? 'https://ipfs.filebase.io/ipfs/QmZ3xMFpfF5q6bT1WDv9eGCCswDKzf69ZwYrEHUMJcmV2c/QmbPMLL9ccr3Bf769zGT2EaDpHmy3VHAMQpWMc8x9zVdTx' : 'https://ipfs.filebase.io/ipfs/QmQNn2LNn6UdzbuqiSSXJpknNF62sDQbzugxGJm2E9cG8b'}
-        alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover"
-      />
+      {productHandle === undefined ? (
+        <img
+          src="https://ipfs.filebase.io/ipfs/QmQNn2LNn6UdzbuqiSSXJpknNF62sDQbzugxGJm2E9cG8b"
+          className="absolute inset-0 -z-10 h-full w-full object-cover"
+        />
+      ) : (
+        imageByHandle.map((image, i) => {
+          return image.imageHandle == productHandle ? (
+            <img
+              key={i}
+              src={image.src}
+              alt={image.alt}
+              className="absolute inset-0 -z-10 h-full w-full object-cover"
+            />
+          ) : null;
+        })
+      )}
 
       <div className="mx-auto max-w-3xl my-36 pb-24">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
