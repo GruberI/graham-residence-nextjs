b2837e778cd8672ff1d6441d8fe97946a7c51399
Issa Gruber <issa.gruber88@gmail.com>
2024-01-12 22:16:56 -0700

    add instagram

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
-import HeroImageText from "@/components/Shop/HeroImageText"
+import HeroImageText from "@/components/Shop/HeroImageText";
 import ArtSection from "@/components/Shop/ArtSection";
 import GoodsSection from "@/components/Shop/GoodsSection";
 import Featured from "@/components/Shop/Featured";
-import StayWithUs from "@/components/Shop/StayWithUs";
+// import StayWithUs from "@/components/Shop/StayWithUs";
 import { getProducts } from "@/lib/shopify";
 import { Suspense } from "react";
 import Carousel from "../components/Carousel/Carousel.jsx";
 import { getCollectionProducts } from "../lib/shopify";
 import Section from "../components/Shop/Section.jsx";
 import { notFound } from "next/navigation";
-import SimpleSlider from '../components/Carousel/ImageCarousel.jsx'
-
+import Instagram from "../components/Shop/Instagram";
 
 export default async function Shop() {
   const products = await getProducts({});
@@ -23,13 +22,13 @@ export default async function Shop() {
 
   if (!productsByTag) return notFound();
 
-  fetch('https://api.instagram.com/oauth/authorize', {
-    method: 'POST',
+  fetch("https://api.instagram.com/oauth/authorize", {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: '?client_id=703131838434878\n&redirect_uri=https://grahamresidence.hirejs.com/\n&scope=user_profile,user_media\n&response_type=code'
-});
+    body: "?client_id=703131838434878\n&redirect_uri=https://grahamresidence.hirejs.com/\n&scope=user_profile,user_media\n&response_type=code",
+  });
 
   return (
     <div className="bg-white overflow-hidden">
@@ -42,7 +41,8 @@ export default async function Shop() {
       <ArtSection products={products} />
       <Featured />
       <GoodsSection products={products} />
-      <StayWithUs />
+      <Instagram />
+      {/* <StayWithUs /> */}
     </div>
   );
 }
---
+++ components/Shop/Instagram.jsx
@@ -0,0 +1,27 @@
+import Calendar from "../Navbar/CalendarWidget/Calendar";
+
+export default function Instagram() {
+  return (
+    <div className="max-w-7xl m-auto border-t border-black">
+      <div className="flex flex-col justify-center pt-20">
+        <p className="font-thin sm:text-3xl text-2xl leading-8 flex sm:justify-center justify-start">
+          {" "}
+          WE'D LOVE TO SEE YOU AT THE GRAHAM RESIDENCE
+        </p>
+
+        <div className="mb-14 mt-6 m-auto">
+          <Calendar />
+        </div>
+      </div>
+      <script src="https://snapwidget.com/js/snapwidget.js"></script>
+      <iframe
+        src="https://snapwidget.com/embed/1055188"
+        class="snapwidget-widget"
+        allowtransparency="true"
+        frameborder="0"
+        scrolling="no"
+        style={{ border: "none", overflow: "hidden", width: "100%" }}
+      ></iframe>
+    </div>
+  );
+}
