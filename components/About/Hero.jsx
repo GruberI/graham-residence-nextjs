b08a92d4ba6608fe77125772b84b608d95d84155
Issa Gruber <issa.gruber88@gmail.com>
2024-02-21 20:14:31 -0700

    spacing on about banner

--- components/About/Hero.jsx
+++ components/About/Hero.jsx
@@ -1,13 +1,13 @@
 export default function Hero() {
   return (
-    <div className="relative isolate overflow-hidden sm:pt-30 pt-40">
+    <div className="relative isolate overflow-hidden sm:pt-28 pt-40">
       <img
         src="https://ipfs.filebase.io/ipfs/QmYTuXismQUCXVPb6MTPNd1AMwVbi7BjQRYAG6gtHQHymP/QmNqmfpcLzdfj9CeVsURAbP8ZfWGUndiRifrHAp8veYiwa"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
 
-      <div className="mx-auto max-w-4xl py-32 sm:py-30">
+      <div className="mx-auto max-w-4xl py-28 mb-12">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
         <div className="text-center mb-36">
           <p className="font-thin tracking-tight text-white sm:text-3xl text-lg animate-fadeTwo">
