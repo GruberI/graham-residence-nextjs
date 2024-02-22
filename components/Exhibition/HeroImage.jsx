b81f492936de530d38b737d818c6969108a9f07e
Issa Gruber <issa.gruber88@gmail.com>
2024-02-21 19:40:43 -0700

    new gallery hero

--- components/Exhibition/HeroImage.jsx
+++ components/Exhibition/HeroImage.jsx
@@ -2,7 +2,7 @@ export default function HeroImageText() {
   return (
     <div className="relative isolate overflow-hidden pt-36 py-80">
       <img
-        src="https://ipfs.filebase.io/ipfs/QmXC7LZEvKiPjhVksbpX2ms16FbgD6neTyB8UXqQ6FZRXm/QmYWubYicxgChQpbsui5FoiyW48Y4HDvxRfE3v4B56vSAi"
+        src="https://ipfs.filebase.io/ipfs/QmdcKQBRdXHif8GvZt9itMrr7HLUs1ciAQp8Cfyym2WyrC/QmY1vkcE3vKoVhKyYZ89B1w3UioNetz8uT12JHjtajbAzF"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
