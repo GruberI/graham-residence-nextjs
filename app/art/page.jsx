c34924e3481fb1ef70781a588a4457284a78f533
Issa Gruber <issa.gruber88@gmail.com>
2024-02-26 17:50:42 -0700

    remove art banner

--- app/art/page.jsx
+++ app/art/page.jsx
@@ -1,7 +1,6 @@
 import ShopAllArt from "../../components/ShopAll/Art/Art";
 import { getCollectionProducts } from "../../lib/shopify";
 import Contact from "@/components/Contact";
-import ShopAllArtHero from "../../components/ShopAll/Art/Hero";
 import { notFound } from "next/navigation";
 
 export const runtime = "edge";
@@ -13,7 +12,6 @@ export default async function ShopAll() {
 
   return (
     <div className="mt-20">
-      <ShopAllArtHero pageTitle="All Artwork" />
       <ShopAllArt productsByHandle={products} />
       <div className="pb-20">
         <Contact />
