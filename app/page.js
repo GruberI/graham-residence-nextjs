4e3df8e8d64a453ad291834c695678bc8e3ba00c
Issa Gruber <issa.gruber88@gmail.com>
2024-03-01 13:51:23 -0700

    popup timing

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-// import Header from "../components/Property/Header";
-// import HeaderImage from "../components/Property/HeaderImage";
-// import Navigation from '../components/Property/Navigation'
 import SectionFirst from "../components/Property/SectionFirst";
 import TwoColumnGrid from "../components/Property/TwoColumnGrid";
 import Text from "../components/Property/Text";
-// import FourImageGrid from "../components/Property/FourImageGrid";
 import TwoRowGrid from "../components/Property/TwoRowGrid";
 import Book from "../components/Property/Book";
 import HeroImageText from "../components/Property/HeroImageText";
@@ -13,6 +9,7 @@ import HouseSpecs from "../components/Property/HouseSpecs";
 import Modal from "../components/Modal";
 
 export default function Property() {
+
   return (
     <div>
       {/* <Header />
