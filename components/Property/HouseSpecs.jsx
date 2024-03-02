e3c616511ad625e450567dd0872844b50df49f9d
Issa Gruber <issa.gruber88@gmail.com>
2024-03-01 20:13:21 -0700

    padding

--- components/Property/HouseSpecs.jsx
+++ components/Property/HouseSpecs.jsx
@@ -1,7 +1,7 @@
 export default function HouseSpecs() {
   return (
     <div className="mt-4 sm:mt-16">
-      <div className="m-auto mx-10 sm:pt-0 pt-10 pb-10 sm:pb-0">
+      <div className="m-auto mx-10 sm:pt-0 pt-10 pb-10">
         <div className="mx-auto px-4 sm:px-6 lg:px-6">
         <img
             src="https://ipfs.filebase.io/ipfs/Qmc4bptgemp9gX5xD7A9jpoMVATyYHLNPmfu4zr4s35UbQ/QmX7SB4rNJmBpmgWg2H9tgcnBZU41fpdRXtPkW2i1NwHvq"
