9e81abedaacd9504f75b02d39fd90648a66d9fc4
Issa Gruber <issa.gruber88@gmail.com>
2024-04-05 19:09:54 -0600

    added google tag on layout

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-M4X900NBP2"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-M4X900NBP2');
+        </script>
         <link rel="shortcut icon" href="./images/favicon_io/icon.ico" />
         <link
           rel="apple-touch-icon"
