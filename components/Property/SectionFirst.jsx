41c162837b4ec65ecb7901125a095b5b815e1c30
Issa Gruber <issa.gruber88@gmail.com>
2024-05-14 22:51:17 -0600

    remove slide aagain

--- components/Property/SectionFirst.jsx
+++ components/Property/SectionFirst.jsx
@@ -1,12 +1,12 @@
-"use client";
-import { Slide } from "react-awesome-reveal";
+// "use client";
+// import { Slide } from "react-awesome-reveal";
 
 export default function SectionFirst() {
   return (
     <div className="bg-[#f4f5f1] w-full" id="property">
       <div className="m-auto mx-10 sm:pt-0 pt-10 pb-10 sm:pb-0">
         <div className="mx-auto px-4 sm:px-6 lg:px-6">
-          <div className="grid grid-cols-1 items-center gap-x-10  lg:grid-cols-2">
+          <div className="grid grid-cols-1 items-center gap-x-10 lg:grid-cols-2">
             <div>
               <div className="overflow-hidden bg-black-100 aspect-[4/5] sm:py-10 lg:py-32 sm:px-20 lg:px-16">
                 <img
@@ -18,7 +18,7 @@ export default function SectionFirst() {
             </div>
             <div>
               <div className="sm:space-y-6 space-y-10">
-                <Slide direction="right">
+                {/* <Slide direction="right"> */}
                   <p className="tracking-wide text-xl sm:text-3xl xl:text-5xl text-center sm:text-left font-thin sm:mt-6 mt-10 mb-4 sm:mb-10 px-0 sm:px-16 lg:px-0">
                     The Graham Residence is a mid-century modern retreat near
                     Joshua Tree National Park where the lines between home and
@@ -32,7 +32,7 @@ export default function SectionFirst() {
                   >
                     Book a stay
                   </a>
-                </Slide>
+                {/* </Slide> */}
               </div>
             </div>
           </div>
