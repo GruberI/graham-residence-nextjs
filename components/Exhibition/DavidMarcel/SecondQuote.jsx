c1e8984d288beee9043d37751adb21a20ad99e08
matt <matt@wip.fyi>
2024-05-30 16:24:35 -0700

    Spacing + Missing Period

--- components/Exhibition/DavidMarcel/SecondQuote.jsx
+++ components/Exhibition/DavidMarcel/SecondQuote.jsx
@@ -1,15 +1,15 @@
 export default function SecondQuote() {
-    return (
-      <div className="m-auto sm:mt-20 mt-10 max-w-7xl">
-        <div className="mx-auto text-center">
-          <div className="inset-x-0 bottom-0 sm:static sm:self-stretch border-t border-b border-black py-16 px-10">
-            <h1 className="sm:text-2xl text-lg italic tracking-widest font-thin">
-              I try to translate depth and intuition to the viewer in the hopes we can have a conversation and a connection
-            </h1>
-            <p className="pt-8 text-medium">Marcel Rozek</p>
-          </div>
+  return (
+    <div className="m-auto sm:mt-20 mt-10 max-w-7xl">
+      <div className="mx-auto text-center">
+        <div className="inset-x-0 bottom-0 sm:static sm:self-stretch border-t border-b border-black py-16 px-10">
+          <h1 className="sm:text-2xl text-lg italic tracking-widest font-thin">
+            I try to translate depth and intuition to the viewer in the hopes we
+            can have a conversation and a connection.
+          </h1>
+          <p className="pt-8 text-medium">Marcel Rozek</p>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- components/Exhibition/DavidMarcel/ThreeImageSection.jsx
+++ components/Exhibition/DavidMarcel/ThreeImageSection.jsx
@@ -1,7 +1,11 @@
 export default function ThreeImageSection({ imageData }) {
   return (
     <div className="three-image-section mx-auto grid grid-rows-1 grid-cols-1 sm:grid-cols-5 grid-flow-col max-w-7xl py-16">
-      <div className="sm:row-start-1 sm:row-end-3 sm:col-span-2 hidden sm:flex flex-col sm:justify-between">
+      <div
+        className={`sm:row-start-1 ${
+          imageData.imgThird ? "sm:row-end-3" : ""
+        }  sm:col-span-2 hidden sm:flex flex-col sm:justify-between`}
+      >
         <div>
           <a href={imageData.href}>
             <img src={imageData.imgSecond.imgSrc} alt="" />
@@ -16,7 +20,11 @@ export default function ThreeImageSection({ imageData }) {
         )}
       </div>
       <div className="flex flex-col-reverse sm:flex-row  sm:col-span-3 mx-10 sm:mx-auto sm:h-[80%]">
-        <div className="sm:row-start-2 sm:row-span-2 sm:content-end text-end pl-20 pr-4 justify-center mt-6 sm:mt-0">
+        <div
+          className={`${
+            imageData.imgThird ? "sm:row-start-2 sm:row-span-2" : ""
+          } sm:content-end text-end pl-20 pr-4 justify-center mt-6 sm:mt-0`}
+        >
           <a
             href={imageData.href}
             className="flex flex-col space-y-1 sm:space-y-3"
