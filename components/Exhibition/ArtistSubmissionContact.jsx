407f4e30f2d04718ab71b0000e45fe29fd251ed3
Issa Gruber <issa.gruber88@gmail.com>
2024-05-31 08:12:56 -0600

    artist submission padding

--- components/Exhibition/ArtistSubmissionContact.jsx
+++ components/Exhibition/ArtistSubmissionContact.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ArtistSubmissionContact() {
   return (
-    <div className="mt-10 sm:mt-10 m-auto flex justify-center flex-col border-t border-black pt-20">
+    <div className="m-auto flex justify-center flex-col border-t border-black py-16">
       <h2 className="sm:text-4xl text-3xl text-center pb-2">
         Artist Submission
       </h2>
