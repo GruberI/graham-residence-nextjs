0942d7dac4fb17766eadec713996c20647a96947
Issa Gruber <issa.gruber88@gmail.com>
2024-06-21 11:31:21 -0600

    mobile carousel image aspects

--- components/Product/ProductDescription/SimpleSlider.jsx
+++ components/Product/ProductDescription/SimpleSlider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 
 function SimpleSlider({ images }) {
+  console.log("images", images);
   const settings = {
     dots: false,
     infinite: true,
@@ -13,7 +14,14 @@ function SimpleSlider({ images }) {
     <div className="slider-container mx-4">
       <Slider {...settings}>
         {images.map((image, i) => {
-          return <img src={image.url} alt="" key={i} className="px-2" />;
+          return (
+            <img
+              src={image.url}
+              alt=""
+              key={i}
+              className="aspect-[3/4] object-cover px-1"
+            />
+          );
         })}
       </Slider>
     </div>
