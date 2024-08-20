ef76e2ca8d124dcde20d6d246b8075cf76c75682
Issa Gruber <issa.gruber88@gmail.com>
2024-08-19 21:48:12 -0600

    fix ratios

--- components/Property/PhotoGallery.jsx
+++ components/Property/PhotoGallery.jsx
@@ -26,35 +26,35 @@ export default function PhotoGallery() {
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmUkoxp4LeUoxmVCnWnHR4LSnP9yUDpqbWsNzRgqD57fko"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmSMCrP1H299mhwBiWj8MWMh2gJPtQEfUEDDvrfsRPNjJ7"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmVZ5hXxf5StvDqXxYD5Nd3zwdw5zoBCG7JgoXNnS4ng1V"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmehgyBPaHAGyYVNLbGK4y3PdbHVFpe73QUwumjFGG6Xjs"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmUSvbboQFikoE66MD4fSeWyvK8y8kHxhf9FBfarjLUNRB"
             alt="gallery-photo"
           />
@@ -64,35 +64,35 @@ export default function PhotoGallery() {
       <div class="grid gap-4">
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmYX2HwzZLLBiQvM48dVjX59qkpMtofdU4BkcUxKkS7tZY"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/Qmdg7UHFUDUHN53Yncai6pVcWWEWaSbCWsJWMvYYUd1YcS"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center "
+            class="h-auto max-w-full object-cover object-center "
             src="https://ipfs.filebase.io/ipfs/QmQxyWSaFMFNUGVEzmuHk8yKTURVUVT8oW7d4scj8nL2A3"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmavuRWAPfXJPDAjBbuCViML3JHC8EKBwXGdigQ61gV4Jc"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/Qmf8nxMk9fu7L7iXvpn7YJ9HMwWa8FrZh97LYZCKNt4LrP"
             alt="gallery-photo"
           />
@@ -106,14 +106,14 @@ export default function PhotoGallery() {
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmUSvbboQFikoE66MD4fSeWyvK8y8kHxhf9FBfarjLUNRB"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmSnByY3oja5EscVvzuD4UqxF5e1mjafLKWt7Miu3uhAgv"
             alt="gallery-photo"
           />
@@ -123,35 +123,35 @@ export default function PhotoGallery() {
       <div class="grid gap-4">
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmZFfYUtpqaevgpovXEia4Qwx4iEE6gQ8fdKXBNjHMiJ9N"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center "
+            class="h-auto max-w-full object-cover object-center "
             src="https://ipfs.filebase.io/ipfs/QmXFEsSuaftfYqRGTcvEJvBox6x2X1PspoJ48W3KDou1Ck"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/Qmacre1JxjPmt1vwstM4PMyVVkWZpbhc7F6AZ84Fi82Dwb"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmWRxHH8LrojYFVzJiQUWugb1sZKY1GCdNHb3KGcBNAuBD"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            class="h-full max-w-full object-cover object-center"
+            class="h-auto max-w-full object-cover object-center"
             src="https://ipfs.filebase.io/ipfs/QmP57Dksypdce4k6GWC5sEXmYdqxhmmqqFq9gqqziWxzPj"
             alt="gallery-photo"
           />
