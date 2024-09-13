5a76f5006d1a84528ad2f4d965d0c5df2e534e82
Studio <studio@Studios-MacBook-Pro.local>
2024-09-13 09:02:44 -0500

    adtocartbutton

--- components/Cart/add-to-cart.tsx
+++ components/Cart/add-to-cart.tsx
@@ -12,10 +12,12 @@ export function AddToCart({
   variants,
   availableForSale,
   type,
+  product, // Add product as a prop
 }: {
   variants: ProductVariant[];
   availableForSale: boolean;
   type: string;
+  product?: { tags: string[] }; // Define the expected structure of the product
 }) {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -33,20 +35,30 @@ export function AddToCart({
     ? "Please select options"
     : undefined;
 
+  // Safeguard against product being undefined
+  const isOnHold = product?.tags?.includes("on-hold");
+
+  // Determine the button text based on availability and tags
+  const buttonText =
+    !availableForSale && isOnHold
+      ? "On Hold"
+      : !availableForSale && type === "art"
+      ? "Sold"
+      : !availableForSale
+      ? "Out Of Stock"
+      : "Add To Cart";
+
   return (
     <button
       aria-label="Add item to cart"
       disabled={isPending || !availableForSale || !selectedVariantId}
       title={title}
       onClick={() => {
-        // Safeguard in case someone messes with `disabled` in devtools.
         if (!availableForSale || !selectedVariantId) return;
-        // **** WHERE I SWITCHED THE CODE FROM ORIGINAL *** //
         startTransition(async () => {
           const error = await addItem(selectedVariantId);
 
           if (error) {
-            // Trigger the error boundary in the root error.js
             throw new Error(error.toString());
           }
 
@@ -54,7 +66,7 @@ export function AddToCart({
         });
       }}
       className={clsx(
-        "m-auto relative flex lg:w-11/12 items-center justify-center border border-black px-4 py-2 text-base font-light  text-black hover:bg-black hover:black focus:outline-none focus:ring-2 focus:ring-stone-600 focus:ring-offset-2 focus:ring-offset-black-50 hover:text-white hover:font-medium sm:w-full",
+        "m-auto relative flex lg:w-11/12 items-center justify-center border border-black px-4 py-2 text-base font-light text-black hover:bg-black hover:black focus:outline-none focus:ring-2 focus:ring-stone-600 focus:ring-offset-2 focus:ring-offset-black-50 hover:text-white hover:font-medium sm:w-full",
         {
           "cursor-not-allowed opacity-60 hover:opacity-60":
             !availableForSale || !selectedVariantId,
@@ -65,14 +77,7 @@ export function AddToCart({
       <div className="absolute left-0 ml-4">
         {!isPending ? <span /> : <LoadingDots className="mb-3 bg-white" />}
       </div>
-      <span className="text-xl">
-        {" "}
-        {availableForSale
-          ? "Add To Cart"
-          : !availableForSale && type === "art"
-          ? "Sold"
-          : "Out Of Stock"}
-      </span>
+      <span className="text-xl">{buttonText}</span>
     </button>
   );
 }
